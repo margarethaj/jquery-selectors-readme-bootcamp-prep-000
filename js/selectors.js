2216ae991985984e94edb8ef77f2e76c8fea2b62
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-23 17:58:58 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,8 @@
 // declare your functions here...
-$(document).ready(function(){
-
-  function paragraphSelector(){
-    return $('p');
-  }
-
-})
-
 function paragraphSelector(){
   return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
 }
